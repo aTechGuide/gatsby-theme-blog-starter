b45b5595197ac4f7fcce3f71047f32881d1a659c
Kamran Ali <kamranalinitb@gmail.com>
2019-07-12 08:36:56 +0530

    Added social share button

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -7,10 +7,14 @@ import Img from 'gatsby-image';
 import authors from '../util/authors';
 import {slugify} from '../util/UtilityFunctions';
 
-const singlepost = ({data}) => {
+const singlepost = ({data, pageContext}) => {
 
   const post = data.markdownRemark.frontmatter;
   const author = authors.find(x => x.name === post.author)
+
+  const baseURL = 'https://kamranali.in/'
+
+
   return (
     <Layout pageTitle={post.title} postAuthor={author} authorImageFluid={data.file.childImageSharp.fluid}>
       <SEO title={post.title} />   
@@ -33,6 +37,15 @@ const singlepost = ({data}) => {
             </ul>
           </CardBody>
         </Card>
+        <h3 className="text-center">
+          Share this Post
+        </h3>
+        <div className="text-center social-share-links">
+          <ul>
+            <li><a href={'http://www.facebook.com/sharer/sharer.php?u=' + baseURL + pageContext.slug} className="facebook" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook fa-2x"></i></a></li>
+            <li><a href={'http://twitter.com/share/?url=' + baseURL + pageContext.slug + '&text=' + post.title + '&via' + 'aTechGuide'} className="twitter" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-2x"></i></a></li>
+          </ul>
+        </div>
     </Layout>
   );
 }
