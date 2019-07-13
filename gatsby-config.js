c35e799f3ad8ca5280360de4cbddccc076179d05
Kamran Ali <kamranalinitb@gmail.com>
2019-07-13 13:25:52 +0530

    Moving posts outside the pages folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,13 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -43,7 +43,7 @@ const singlepost = ({data, pageContext}) => {
         <div className="text-center social-share-links">
           <ul>
             <li><a href={'http://www.facebook.com/sharer/sharer.php?u=' + baseURL + pageContext.slug} className="facebook" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook fa-2x"></i></a></li>
-            <li><a href={'http://twitter.com/share/?url=' + baseURL + pageContext.slug + '&text=' + post.title + '&via' + 'aTechGuide'} className="twitter" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-2x"></i></a></li>
+            <li><a href={'http://twitter.com/share/?url=' + baseURL + pageContext.slug + '&text=' + post.title + '&via=aTechGuide'} className="twitter" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-2x"></i></a></li>
           </ul>
         </div>
     </Layout>
