4917f3c4e937a486243498cef91c5a3883c05342
Kamran Ali <kamranalinitb@gmail.com>
2019-07-13 06:34:44 +0530

    Adding Dynamic Template Page

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,7 @@
 const {slugify} = require('./src/util/UtilityFunctions')
 const path = require('path')
 const authors = require('./src/util/authors')
+const _ = require('lodash')
 
 exports.onCreateNode = ({node, actions}) => {
   const {createNodeField} = actions
@@ -25,7 +26,11 @@ exports.onCreateNode = ({node, actions}) => {
 
 exports.createPages = ({actions, graphql}) => {
   const {createPage} = actions;
-  const singlePostTemplate = path.resolve('src/templates/single-post.js');
+
+  const templates = {
+    singlePost: path.resolve('src/templates/single-post.js'),
+    tagsPage: path.resolve('src/templates/tags-page.js')
+  }
 
   return graphql(`
   {
@@ -38,6 +43,7 @@ exports.createPages = ({actions, graphql}) => {
           }
           frontmatter {
             author
+            tags
           }
         }
       }
@@ -51,12 +57,40 @@ exports.createPages = ({actions, graphql}) => {
       posts.forEach(({node}) => {
         createPage({
           path: node.fields.slug,
-          component: singlePostTemplate,
+          component: templates.singlePost,
           context: {
             slug: node.fields.slug,
             imageUrl: authors.find(x => x.name === node.frontmatter.author).imageUrl
           }
         })
       })
+
+      let tags = []
+      _.each(posts, edge => {
+        if(_.get(edge, 'node.frontmatter.tags')) {
+          tags = tags.concat(edge.node.frontmatter.tags)
+        }
+      })
+
+      let tagPostCounts = {}
+      tags.forEach(tag => {
+        tagPostCounts[tag] = (tagPostCounts[tag] || 0) + 1;
+      })
+      // {code: 2, design: 6, ...}
+
+      tags = _.uniq(tags);
+
+      console.log(tags);
+      
+
+      createPage({
+        path: '/tags',
+        component: templates.tagsPage,
+        context: {
+          tags,
+          tagPostCounts
+        }
+      })
+
   })
 }
---
+++ src/templates/tags-page.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import {Badge, Button} from 'reactstrap';
+import {slugify} from '../util/UtilityFunctions';
+
+const tagsPage = ({pageContext}) => {
+
+  const {tags, tagPostCounts} = pageContext;
+
+  return (
+    <Layout pageTitle="All Tags">
+      <SEO title="All Tags" />
+      <ul>
+        {tags.map(tag => (
+          <li key={tag} style={{marginBottom: '10px'}}> 
+            <Button color="primary" href={`/tag/${slugify(tag)}`}>
+              {tag} <Badge color="light"> {tagPostCounts[tag]} </Badge>
+            </Button>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
+
+export default tagsPage;
