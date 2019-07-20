0c2ea19ab76b1f930cf3011509d4a9a7f895b366
Kamran Ali <kamranalinitb@gmail.com>
2019-07-20 21:43:13 +0530

    commented manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,18 +31,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     `gatsby-transformer-remark`,
     {
       resolve: 'gatsby-plugin-material-ui',
