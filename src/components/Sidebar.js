ff4f7c4995a75a77ce53eeae25d8f16983e7ff17
Kamran Ali <kamranalinitb@gmail.com>
2019-07-20 12:38:06 +0530

    Added Grid and Sidebar to Single Post Page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,104 +1,33 @@
 import React from 'react';
-import { Card, CardTitle, CardBody, CardText, Form, FormGroup, Input} from 'reactstrap';
-import { graphql, StaticQuery, Link } from 'gatsby';
+import {CardHeader, Card, CardContent, TextField} from '@material-ui/core';
 
-import Img from 'gatsby-image';
-
-
-const Sidebar = ({author, authorFluid}) => {
+const Sidebar = () => {
   return (
     <div>
-      {author && (
-        <Card>
-          <Img className="card-img-top" fluid={authorFluid} />
-          <CardBody>
-            <CardTitle className="text-center text-uppercase mb-3">
-              {author.name}
-            </CardTitle>
-            <CardText> {author.bio} </CardText>
-            <div className="author-social-links text-center">
-              <ul>
-                <li><a href={author.facebook} target="_blank" rel="noopener noreferrer" className="facebook"> <i className="fa fa-facebook fa-2x"></i></a></li>
-                <li><a href={author.twitter} target="_blank" rel="noopener noreferrer" className="twitter"><i className="fa fa-twitter fa-2x"></i></a> </li>
-                <li><a href={author.instagram} target="_blank" rel="noopener noreferrer" className="instagram"><i className="fa fa-instagram fa-2x"></i></a></li>
-              </ul>
-            </div>
-          </CardBody>
-        </Card>
-      )}
       <Card>
-        <CardBody>
-          <CardTitle className="text-center text-uppercase mb-3">
-            NewsLetter
-          </CardTitle>
-          <Form className="text-center">
+        <CardHeader
+          title="Subscribe"
+          />
+          
+        <CardContent>
+          {/* <Form className="text-center">
             <FormGroup>
               <Input type="email" name="email" placeholder="Your email address.." />
             </FormGroup>
             <button className="btn btn-outline-success text-uppercase"> Subscribe </button>
-          </Form>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <CardTitle className="text-center text-uppercase">
-            Advertisement
-          </CardTitle>
-          <img src="https://via.placeholder.com/320x200" alt="Advert" style={{width: "100%"}}/>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardBody>
-          <CardTitle className="text-center text-uppercase mb-3">
-            Recent Posts
-          </CardTitle>
-          <StaticQuery query={sidebarQuery} render={ (data) => (
-            <div>
-              {data.allMarkdownRemark.edges.map( ({node}) => (
-                <Card key={node.id} >
-                   <Link to={node.fields.slug}>
-                     <Img className="card-img-top" fluid={node.frontmatter.image.childImageSharp.fluid} />   
-                   </Link>
-                   <CardBody>
-                     <CardTitle>
-                       <Link to={node.fields.slug}>
-                         {node.frontmatter.title}
-                       </Link>
-                     </CardTitle>
-                   </CardBody>
-                </Card>
-              ) )}
-            </div>
-          ) }/>
-        </CardBody>
+          </Form> */}
+          <form noValidate autoComplete="off">
+            <TextField
+              id="outlined-name"
+              label="Email"
+              margin="normal"
+              variant="outlined"
+            />
+          </form>
+        </CardContent>
       </Card>
     </div>
   );
 }
-
-const sidebarQuery = graphql`
-{
-  allMarkdownRemark (sort: {fields:[frontmatter___date], order:DESC}, limit: 3)
-   { edges {
-      node {
-        id
-        fields {
-          slug
-        }
-        frontmatter {
-          title
-          image {
-            childImageSharp {
-              fluid(maxWidth: 300) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-`
 
 export default Sidebar;
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Layout from '../components/layout';
 import { graphql, Link } from 'gatsby';
 import SEO from '../components/seo';
-import {Avatar, Typography, CardHeader, Card, CardActionArea,CardActions,CardContent, Chip} from '@material-ui/core';
+import {Avatar, CardHeader, Card, CardActionArea,CardActions,CardContent, Chip, Grid} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Img from 'gatsby-image';
 import authors from '../util/authors';
 import {slugify} from '../util/UtilityFunctions';
+import Sidebar from '../components/Sidebar';
 
 const useStyles = makeStyles(theme => ({
   chiprow: {
@@ -23,59 +24,102 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const singlepost = ({data, pageContext}) => {
+const PostCard = ({data, post}) => {
   const classes = useStyles();
 
+  return (
+    <Card>
+      <CardActionArea>
+        <Img className="card-image-top" fluid={post.image.childImageSharp.fluid}/>
+        <CardHeader
+          avatar={<Avatar aria-label="Recipe" className={classes.avatar}>AB</Avatar>}
+          title={post.title}
+          subheader={post.date}
+        />
+        <CardContent>    
+          <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.chiprow}>
+        <div >
+          {post.tags.map(tag => (
+            <Link key={tag} to={`/tag/${slugify(tag)}`}>
+              <Chip size='small' color='primary' label={tag} className={classes.chip} />
+            </Link>
+            ))}
+        </div>
+      </CardActions>
+    </Card>
+  );
+}
+
+const Sharing = ({pageContext, post}) => {
+  
+  const baseURL = 'https://kamranali.in/'
+
+  return (
+    <>
+      <h3 className="text-center">
+        Share this Post
+      </h3>
+      <div className="text-center social-share-links">
+        <ul>
+          <li><a href={'http://www.facebook.com/sharer/sharer.php?u=' + baseURL + pageContext.slug} className="facebook" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook fa-2x"></i></a></li>
+          <li><a href={'http://twitter.com/share/?url=' + baseURL + pageContext.slug + '&text=' + post.title + '&via=aTechGuide'} className="twitter" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-2x"></i></a></li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+const singlepost = ({data, pageContext}) => {
+  
   const post = data.markdownRemark.frontmatter;
   const author = authors.find(x => x.name === post.author)
 
-  const baseURL = 'https://kamranali.in/'
-
-
   return (
     <Layout pageTitle={post.title} postAuthor={author} authorImageFluid={data.file.childImageSharp.fluid}>
-      <SEO title={post.title} />   
-        <Card className={classes.card}>
-          <CardActionArea>
-            <Img className="card-image-top" fluid={post.image.childImageSharp.fluid}/>
-            <CardHeader
-            avatar={
-              <Avatar aria-label="Recipe" className={classes.avatar}>
-                AB
-              </Avatar>
-            }
-            title={post.title}
-            subheader={post.date}
-            />
-            <CardContent>    
-              <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-            </CardContent>
-          </CardActionArea>
-          <CardActions className={classes.chiprow}>
-            <div >
-              {post.tags.map(tag => (
-                <Link key={tag} to={`/tag/${slugify(tag)}`}>
-                  <Chip size='small' color='primary' label={tag} className={classes.chip} />
-                </Link>
-                ))}
-            </div>
-          </CardActions>
-        </Card>
-        <h3 className="text-center">
-          Share this Post
-        </h3>
-        <div className="text-center social-share-links">
-          <ul>
-            <li><a href={'http://www.facebook.com/sharer/sharer.php?u=' + baseURL + pageContext.slug} className="facebook" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook fa-2x"></i></a></li>
-            <li><a href={'http://twitter.com/share/?url=' + baseURL + pageContext.slug + '&text=' + post.title + '&via=aTechGuide'} className="twitter" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter fa-2x"></i></a></li>
-          </ul>
-        </div>
+      <SEO title={post.title} />
+
+        {/* Main Container */}
+        <Grid container spacing={data.site.siteMetadata.gridSpacing}>
+          
+          <Grid item xs={12} md={9}>
+            {/* Left Container Start */}
+            <Grid container spacing={data.site.siteMetadata.gridSpacing} >
+              <Grid item xs={12} >
+                <PostCard post={post} data={data}/>
+              </Grid>
+              <Grid item xs={12}>
+                <Sharing pageContext={pageContext} post={post}/>
+              </Grid>
+            </Grid> 
+            {/* Left Container End */}      
+          </Grid>
+
+          <Grid item xs={12} md={3}>
+            {/* Right Container Start*/}
+            <Grid container spacing={data.site.siteMetadata.gridSpacing} >
+              <Grid item xs={12}>
+                <Sidebar />
+              </Grid>
+            </Grid>
+            {/* Right Container End*/}
+          </Grid>
+        
+        </Grid>
+             
     </Layout>
   );
 }
 
 export const postQuery = graphql`
   query blogPostBySlug($slug: String!, $imageUrl: String!) {
+    site {
+      siteMetadata {
+        gridSpacing
+      }
+    }
     markdownRemark(fields: { slug: {eq: $slug}}) {
       id
       html
