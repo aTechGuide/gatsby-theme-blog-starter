26fb255f59eb38d41825d07331d0af2476728ee0
Kamran Ali <kamranalinitb@gmail.com>
2019-08-01 23:07:40 +0530

    Adding netlify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,6 +95,7 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    ` gatsby-plugin-netlify-headers`,
   ],
 }
