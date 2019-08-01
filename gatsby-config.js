8d10dd4ccaea380a8a265bf001d906b8530541e9
Kamran Ali <kamranalinitb@gmail.com>
2019-08-01 22:36:12 +0530

    Added Manifest + offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,20 +82,19 @@ module.exports = {
         // },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ArabicBlog`,
+        short_name: `ArabicBlog`,
+        description: `Arabic Notes`,
+        start_url: `/`,
+        background_color: `#673ab7`,
+        theme_color: `#673ab7`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`
   ],
 }
