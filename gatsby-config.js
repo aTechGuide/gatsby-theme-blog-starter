66307d35fda9749abcadeb3d2d1cd22774996236
Kamran Ali <kamranalinitb@gmail.com>
2019-08-05 08:34:44 +0530

    Script src = unsafe-inline for gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 const cspDirectives = [
   "default-src 'self'",
-  "script-src 'self' https://www.google-analytics.com",
+  "script-src 'self' 'unsafe-inline' https://www.google-analytics.com",
   "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "img-src 'self' data: https://www.google-analytics.com",
