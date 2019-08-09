b7849d01f45bee39c9326648ae33dd857e3984a2
Kamran Ali <kamranalinitb@gmail.com>
2019-08-09 23:17:19 +0530

    a2hs original 2

--- src/components/seo/Seo.js
+++ src/components/seo/Seo.js
@@ -228,40 +228,7 @@ function Seo({ title, description, tags, image: metaImage, isBlogPost, slug, dat
       )
       .concat(meta)}
     >
-      <script type="text/javascript">
-        {`
-          // This JS file is minified and used in Seo.js
-
-          function addToHomeScreen() { 
-            var a2hsBtn = document.querySelector("#a2hs");
-            a2hsBtn.style.display = "none";
-            deferredPrompt.prompt();
-              // .then(
-              //   result => alert(result), 
-              //   error => alert(error)
-              // );
-            deferredPrompt.userChoice
-              .then(function(choiceResult){
-            if (choiceResult.outcome === 'accepted') {
-              console.log('User accepted the A2HS prompt');
-            } else {
-              console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-          });}
-          
-          var deferredPrompt;
-          
-          window.addEventListener('beforeinstallprompt', function (e) {
-            e.preventDefault();
-            deferredPrompt = e;
-            
-            var a2hsBtn = document.querySelector("#a2hs");
-            a2hsBtn.style.display = "block";
-            a2hsBtn.addEventListener("click", addToHomeScreen);
-          });
-        `}
-      </script>
+      <script type="text/javascript" src="/script.js" />
       <script type="application/ld+json">{JSON.stringify(schema)}</script>
 
       <link rel="canonical" href={url} />
