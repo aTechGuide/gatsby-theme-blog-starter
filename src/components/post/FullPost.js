6f13ddc6c925465cf7684c81fd08ddbba45c2e2f
Kamran Ali <kamranalinitb@gmail.com>
2019-09-05 00:23:31 +0530

    refactored post theme + added table css to post theme

--- src/components/post/FullPost.js
+++ src/components/post/FullPost.js
@@ -6,6 +6,7 @@ import Img from "gatsby-image"
 
 import Context from '../Context';
 import Tags from './Tags';
+import postTheme from '../../styles/postTheme';
 
 /**
  dt, .word{color: blue;}
@@ -19,89 +20,7 @@ dt, dd {font-weight: bold}
 .bg-cyan{background-color: lightcyan;}
 .bg-green{background-color: lightgreen;}
  */
-const useStyles = makeStyles(theme => ({
-  
-  chip: {
-    margin: theme.spacing(1),
-  },
-  card: {
-    maxWidth: '80%',
-    margin: 'auto'
-  },
-  header: {
-    paddingBottom: '0px'
-  },
-  '@global': {
-    '.firstword': {
-      color: theme.palette.primary[500],
-      fontSize: theme.spacing(3)
-    },
-    '.word' : { //<- Deprecate this
-      color: theme.palette.primary[500]
-    },
-    '.irab' : {
-      color: theme.palette.primary[500],
-      display: 'block'
-    },
-    '.irabhighlight': {
-      color: theme.headingColor[500]
-    },
-    '.heading1': {
-      color: theme.headingColor[900]
-    },
-    '.heading2' : {
-      color: theme.headingColor[400]
-    },
-    '.exception' : {
-      color: theme.palette.error['dark'],
-      display: 'inline'
-    },
-    '.bg-yellow' : {
-      backgroundColor: theme.highlightOne[200],
-      borderRadius: theme.spacing(.5)
-    },
-    '.bg-cyan' : {
-      backgroundColor: theme.highlightTwo[50],
-      borderRadius: theme.spacing(.5)
-    },
-    '.bg-green' : {
-      backgroundColor: theme.highlightThree[100],
-      borderRadius: theme.spacing(.5)
-    },
-    'h2' : {
-      fontSize: "1.5rem",
-      fontWeight: 500,
-      color: theme.palette.secondary[700] ,
-      margin: 0,
-      marginTop: theme.spacing(2),
-    },
-    'h3' : {
-      fontSize: "1.2rem",
-      fontWeight: 500,
-      color: theme.palette.secondary[700] ,
-      margin: 0,
-      marginTop: theme.spacing(2),
-    },
-    'p' : {
-      margin: 0
-    },
-    'ul' : {
-      margin: 0
-    },
-    'blockquote' : {
-      margin: 0,
-      padding: '0 1rem',
-      borderLeft: '0.25rem solid #ccc',
-      width: 'fit-content',
-      borderTopRightRadius: theme.spacing(1),
-      borderBottomRightRadius: theme.spacing(1),
-      background: theme.palette.grey[200]
-    },
-    '.arabic': {
-      fontFamily :'Markazi Text'
-    }
-  }
-}));
+const useStyles = makeStyles(theme => postTheme(theme));
 
 const FullPost = ({frontmatter, children}) => {
   const classes = useStyles();
---
+++ src/styles/postTheme.js
@@ -0,0 +1,105 @@
+const postTheme = (theme) => ({
+  
+  chip: {
+    margin: theme.spacing(1),
+  },
+  card: {
+    maxWidth: '80%',
+    margin: 'auto'
+  },
+  header: {
+    paddingBottom: '0px'
+  },
+  '@global': {
+    '.firstword': {
+      color: theme.palette.primary[500],
+      fontSize: theme.spacing(3)
+    },
+    '.word' : { //<- Deprecate this
+      color: theme.palette.primary[500]
+    },
+    '.irab' : {
+      color: theme.palette.primary[500],
+      display: 'block'
+    },
+    '.irabhighlight': {
+      color: theme.headingColor[500]
+    },
+    '.heading1': {
+      color: theme.headingColor[900]
+    },
+    '.heading2' : {
+      color: theme.headingColor[400]
+    },
+    '.exception' : {
+      color: theme.palette.error['dark'],
+      display: 'inline'
+    },
+    '.bg-yellow' : {
+      backgroundColor: theme.highlightOne[200],
+      borderRadius: theme.spacing(.5)
+    },
+    '.bg-cyan' : {
+      backgroundColor: theme.highlightTwo[50],
+      borderRadius: theme.spacing(.5)
+    },
+    '.bg-green' : {
+      backgroundColor: theme.highlightThree[100],
+      borderRadius: theme.spacing(.5)
+    },
+    'h2' : {
+      fontSize: "1.5rem",
+      fontWeight: 500,
+      color: theme.palette.secondary[700] ,
+      margin: 0,
+      marginTop: theme.spacing(2),
+    },
+    'h3' : {
+      fontSize: "1.2rem",
+      fontWeight: 500,
+      color: theme.palette.secondary[700] ,
+      margin: 0,
+      marginTop: theme.spacing(2),
+    },
+    'p' : {
+      margin: 0
+    },
+    'ul' : {
+      margin: 0
+    },
+    'blockquote' : {
+      margin: 0,
+      padding: '0 1rem',
+      borderLeft: '0.25rem solid',
+      width: 'fit-content',
+      borderTopRightRadius: theme.spacing(1),
+      borderBottomRightRadius: theme.spacing(1),
+      background: theme.palette.primary[50]
+    },
+    'blockquote > ul' : {
+      paddingLeft : '20px'
+    },
+    '.arabic': {
+      fontFamily :'Markazi Text'
+    },
+    'table' : {
+      border : '1px solid',
+      borderColor: theme.palette.primary[900],
+      borderCollapse: 'collapse'
+    },
+    'thead' : {
+      color : theme.palette.primary[500]
+    },
+    'td, th' : {
+      textAlign : 'center',
+      padding: '.25rem .5rem',
+      border : '1px solid',
+      borderColor: theme.palette.primary[900],
+    },
+    'tbody tr:nth-child(odd) td, tbody tr:nth-child(odd) th' : {
+      backgroundColor : theme.palette.primary[50]
+    }
+  }
+})
+
+export default postTheme
