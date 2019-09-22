9fc33247ef767a9a5945d3ac3049529ba2afd900
Kamran Ali <kamranalinitb@gmail.com>
2019-09-22 18:14:42 +0530

    Added Not Found SVG

--- theme/src/pages/404.js
+++ theme/src/pages/404.js
@@ -2,30 +2,40 @@ import React from "react"
 
 import Layout from "../components/layout/layout"
 import PageLayout from "../components/layout/PageLayout";
-import { Typography, Button } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 import Seo from "../components/seo/Seo";
+import notFound from './not-found.svg';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from "gatsby";
+
+const useStyles = makeStyles(theme => ({
+  svg: {
+    maxWidth: '100%',
+    marginBottom: '30px'
+  }
+}));
 
 const NotFoundPage = () => {
 
-  
-  return(
-  <Layout pageTitle="Oops, Something went wrong">
-    <Seo 
-      title="404"
-      description="Invalid URL"
-      slug="/404"  />
+  const classes = useStyles();
 
-    <PageLayout title="404: Not found">
-      <Typography>
-        You just hit a page that doesn&#39;t exist... the sadness.
-      </Typography>
-      <Button
-        href='/'
-        variant='contained'
-        color='primary'>
-        Go Home
-      </Button>
-    </PageLayout>
+  return(
+    <Layout>
+      <Seo 
+        title="Oops, Something went wrong"
+        description="Invalid URL"
+        slug="/404"  />
+
+      <PageLayout>
+
+        <img className={classes.svg} src={notFound} alt="404" />
+
+        <Typography>
+          You just hit a page that doesn&#39;t exist... the sadness. <Link to="/"> Go Home </Link> 
+        </Typography>
+       
+      </PageLayout>
   </Layout>
 )}
 
